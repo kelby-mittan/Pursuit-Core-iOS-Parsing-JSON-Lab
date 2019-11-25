ed473982e46d71dacdd15a29b8f19b5a1bc41865
Kelby Mittan <kelbymittan@pursuit.org>
2019-11-25 17:43:03 -0500

    populated my json files

---
+++ Parsing-JSON-Lab2/Parsing-JSON-Lab2/ColorsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ColorsViewController.swift
+//  Parsing-JSON-Lab2
+//
+//  Created by Kelby Mittan on 11/25/19.
+//  Copyright © 2019 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ Parsing-JSON-Lab2/Parsing-JSON-Lab2/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parsing-JSON-Lab2
+//
+//  Created by Kelby Mittan on 11/25/19.
+//  Copyright © 2019 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
